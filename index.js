ca64eff45d08f69f9e616475aaac082c86a6b06a
Odette Salvoro <odette@Odettes-MacBook-Pro.local>
2022-01-20 14:33:49 -0500

    Upload Project

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // New Project 
+function readThisFIle(file){
+    const fs = require('fs')
+    const data = fs.readFileSync('data.json', "utf-8")
+return data
+}
+console.log(readThisFIle())
+
+const fs = require("fs");
+fs.readFile("data.json", "utf-8",(err,data) => {
+    if (err) {
+        console.log(err);
+        }
+        console.log(data)
+})
